0fb4173c989f52a77c09b02bdfdf2691748ed415
Cristofer2024 <cri.sanmartincduocuc.cl>
2025-05-23 11:37:40 -0400

    print y for

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ print("print modificado desde github")
 tabla= 5
 for i in range(1,11):
     print(f"{tabla} x {i} = {tabla * i}")
+
+print("esta es una catualizacion")
+for i in range(10):
+    print(f"{i} subiendo")
